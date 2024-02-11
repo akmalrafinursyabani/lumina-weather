3b1596aa9480bb1eacb436fe20f9e1717a8ab6e0
Akmal Rafi Nursya'bani <akmalrafi.yourdev@gmail.com>
2024-02-11 13:09:55 +0700

    first commit

---
+++ app/src/main/java/com/arafinsandbox/luminaweather/ui/theme/Typeface.kt
@@ -0,0 +1,15 @@
+package com.arafinsandbox.luminaweather.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.arafinsandbox.luminaweather.R
+
+val luminaWeatherTypeface = FontFamily(
+    Font(R.font.plusjakartasansregular, weight = FontWeight.Normal),
+    Font(R.font.plusjakartasansitalic, weight = FontWeight.Normal),
+    Font(R.font.plusjakartasanslight, weight = FontWeight.Light),
+    Font(R.font.plusjakartasanslightitalic, weight = FontWeight.Light),
+    Font(R.font.plusjakartasansbold, weight = FontWeight.Bold),
+    Font(R.font.plusjakartasansbolditalic, weight = FontWeight.Bold)
+)
